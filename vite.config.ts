8f3478f6f341840e82651a25e6b4be6ec72fd378
verncat <183478378+verncat@users.noreply.github.com>
2026-01-11 00:21:42 +0400

    Upgrade to TailwindCss v4 and themes!

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { execSync } from "child_process";
 
 // Получаем информацию о билде
@@ -35,7 +36,7 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    plugins: [react()],
+    plugins: [tailwindcss(), react()],
     define: {
       "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
       "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
